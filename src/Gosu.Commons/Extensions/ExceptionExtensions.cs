6bf100b906fe2c709d2c38bd1bb602cd6c951428
Erik Öjebo <erik.ojebo@gmail.com>
2013-04-02 20:08:50 +0200

    Added extension for converting an exception to a log message

---
+++ src/Gosu.Commons/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace Gosu.Commons.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        private const string Indentation = "  ";
+
+        public static string ToLogMessage(this Exception exception)
+         {
+             return exception.ToLogMessage("");
+         }
+
+         private static string ToLogMessage(this Exception exception, string prefix)
+         {
+             var message = new StringBuilder();
+
+             message.AppendLine(prefix + "Message: " + exception.Message);
+             message.AppendLine(prefix + "DateTime: " + DateTime.Now);
+             message.AppendLine(prefix + "StackTrace:");
+             message.AppendLine(prefix + exception.StackTrace);
+
+             if (exception.InnerException != null)
+             {
+                 message.AppendLine(prefix + "Inner exception: --------------------------------------");
+                 message.Append(exception.InnerException.ToLogMessage(prefix + Indentation));
+             }
+
+             return message.ToString();
+         }
+    }
+}
