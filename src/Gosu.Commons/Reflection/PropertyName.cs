1c799859d0a1468a537b9ea428d01f297392ecdd
Erik Öjebo <erik.ojebo@gmail.com>
2013-10-09 08:56:24 +0200

    Added helper class for finding property name from expressions

---
+++ src/Gosu.Commons/Reflection/PropertyName.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Gosu.Commons.Reflection
+{
+    public class PropertyName
+    {
+        public static string For<T>(Expression<Func<T, object>> getterExpression)
+        {
+            return GetPropertyNameFromExpression(getterExpression.Body);
+        }
+
+        public static string For<T, U>(Expression<Func<T, U>> getterExpression)
+        {
+            return GetPropertyNameFromExpression(getterExpression.Body);
+        }
+
+        public static string For<T>(Expression<Func<T>> getterExpression)
+        {
+            return GetPropertyNameFromExpression(getterExpression.Body);
+        }
+
+        private static string GetPropertyNameFromExpression(Expression getterExpression)
+        {
+            if (getterExpression is UnaryExpression)
+                return GetPropertyNameFromExpression(((UnaryExpression)getterExpression).Operand);
+
+            var expression = getterExpression as MemberExpression;
+
+            if (expression == null)
+                throw new InvalidOperationException("Can not get member name from the given expression");
+
+            return expression.Member.Name;
+        }
+    }
+}
---
+++ src/Gosu.Specs/Commons/Reflection/PropertyNameSpecs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Gosu.Commons.Reflection;
+using NUnit.Framework;
+
+namespace Gosu.Specs.Commons.Reflection
+{
+    [TestFixture]
+    public class PropertyNameSpecs
+    {
+        [Test]
+        public void Can_get_name_for_first_level_property()
+        {
+            string actualPropertyName = PropertyName.For<List<int>>(x => x.Count);
+
+            Assert.AreEqual("Count", actualPropertyName);
+        }
+    }
+}
